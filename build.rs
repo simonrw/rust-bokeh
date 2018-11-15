d7c2c6116e0f5c6992fb46b75737dd8d0111876e
Simon Walker <s.r.walker101@googlemail.com>
2018-11-15 20:04:36 +0000

    Simplify building to only build 1.0.1
    Also abstract some of the building to a struct where we can hang member
    functions from.

--- build.rs
+++ build.rs
@@ -4,72 +4,67 @@ extern crate serde_json;
 #[macro_use]
 extern crate quote;
 extern crate proc_macro2;
-extern crate semver;
 
 use proc_macro2::{Ident, Span, TokenStream};
-use semver::Version;
 use serde_json::Value;
 use std::env;
+use std::error::Error;
 use std::fs::{self, File};
 use std::io::Write;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
-static MODELS_SRC_DIR: &str = "model_descriptions";
+type Result<T> = std::result::Result<T, Box<Error>>;
 
-fn main() {
+fn main() -> Result<()> {
     env_logger::init();
-
-    let mut envar_file = File::create("ENVLOG").unwrap();
-    for (key, value) in env::vars() {
-        writeln!(envar_file, "{} {}", key, value);
-    }
 
     // Handle template recompilation
     askama::rerun_if_templates_changed();
 
-    build_models_from_definitions();
-}
-
-fn build_models_from_definitions() {
-    let model_files = fs::read_dir(MODELS_SRC_DIR).expect("fetching_files");
-    for entry in model_files {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        let entry_str = path.to_str().unwrap();
-        let version_str = entry_str.split("/").last().unwrap().replace(".json", "");
-        if let Ok(version) = Version::parse(&version_str) {
-            if version.is_prerelease() {
-                continue;
-            }
-
-            build_model(&path);
-        }
-    }
-}
-
-fn build_model(path: &PathBuf) {
-    let content = fs::read_to_string(path).unwrap();
-    let json_content = serde_json::from_str(&content).unwrap();
-    generate_models(json_content);
-}
-
-fn generate_models(description: Value) {
+    // Build the bindings
     let output_file = Path::new(&env::var("OUT_DIR").unwrap()).join("models.rs");
     let mut outfile = File::create(output_file).unwrap();
-    let d = description.as_object().unwrap();
-
-    for (class_name, body) in d.iter() {
-        let tokens = generate_model(class_name, body);
-        let token_str = tokens.to_string();
-        writeln!(outfile, "{}", token_str);
-    }
+    let model_definition_path = "model_descriptions/1.0.1.json";
+    generate_model_bindings(model_definition_path, &mut outfile)
 }
 
-fn generate_model(class_name: &str, _body: &Value) -> TokenStream {
-    let class_name = Ident::new(class_name, Span::call_site());
+fn generate_model_bindings<P>(filename: P, output_file: &mut File) -> Result<()>
+where
+    P: AsRef<Path>,
+{
+    let content = fs::read_to_string(filename)?;
+    let json_content: Value = serde_json::from_str(&content)?;
+    let d = json_content.as_object().unwrap();
 
-    let tokens = quote! {
-        struct #class_name;
-    };
-    tokens
+    for (class_name, body) in d.iter() {
+        let model_definition = ModelDefinition::new(class_name, body);
+        writeln!(output_file, "{}", model_definition.into_token_stream());
+    }
+    Ok(())
+}
+
+struct ModelDefinition {
+    name: String,
+    body: Value,
+}
+
+impl ModelDefinition {
+    fn new(name: &str, body: &Value) -> ModelDefinition {
+        ModelDefinition {
+            name: name.into(),
+            body: body.clone(),
+        }
+    }
+
+    fn into_token_stream(self) -> TokenStream {
+        let class_name = self.class_name();
+
+        quote! {
+            struct #class_name;
+        }
+    }
+
+    fn class_name(&self) -> Ident {
+        Ident::new(&self.name, Span::call_site())
+    }
 }
