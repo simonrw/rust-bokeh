12d5ae163d23235c95b1ce5ae9e54ddab311ce41
Simon Walker <s.r.walker101@googlemail.com>
2018-11-18 22:24:32 +0000

    Handle test code with test flags

--- bokeh-models/src/to_bokehjs.rs
+++ bokeh-models/src/to_bokehjs.rs
@@ -1,58 +1,45 @@
 use serde_json::Value;
+#[cfg(test)]
 use std::fmt::Display;
 
 pub(crate) trait ToBokehJs {
     fn to_json(&self) -> Value;
 }
 
-#[derive(Default)]
-struct BasicTickFormatter;
-
-impl ToBokehJs for BasicTickFormatter {
-    fn to_json(&self) -> Value {
-        json!({
-            "id": "1363",
-            "js_event_callbacks": {},
-            "js_property_callbacks": {},
-            "name": null,
-            "power_limit_high": 5,
-            "power_limit_low": -3,
-            "precision": "auto",
-            "subscribed_events": [],
-            "tags": [],
-            "use_scientific": true,
-        })
-    }
-}
-
+#[cfg(test)]
 pub(crate) trait ToValue {
     fn to_value(&self) -> Value;
 }
 
+#[cfg(test)]
 impl ToValue for Value {
     fn to_value(&self) -> Value {
         self.clone()
     }
 }
 
+#[cfg(test)]
 impl<'a> ToValue for &'a Value {
     fn to_value(&self) -> Value {
         (*self).clone()
     }
 }
 
+#[cfg(test)]
 impl<'a> ToValue for &'a str {
     fn to_value(&self) -> Value {
         serde_json::from_str(self).unwrap()
     }
 }
 
+#[cfg(test)]
 impl ToValue for String {
     fn to_value(&self) -> Value {
         serde_json::from_str(self).unwrap()
     }
 }
 
+#[cfg(test)]
 impl<'a> ToValue for &'a String {
     fn to_value(&self) -> Value {
         serde_json::from_str(self).unwrap()
@@ -60,6 +47,7 @@ impl<'a> ToValue for &'a String {
 }
 
 // Helper function to compare JSON strings
+#[cfg(test)]
 pub(crate) fn compare_json<A, B>(s1: A, s2: B)
 where
     A: ToValue + Display,
@@ -90,13 +78,5 @@ mod tests {
         let json = Foo.to_json();
         let json_str = serde_json::to_string(&json).unwrap();
         assert_eq!(json_str, "null");
-    }
-
-    #[test]
-    fn test_basic_tick_formatter() {
-        let t = BasicTickFormatter::default();
-
-        let json = t.to_json();
-        compare_json(&json, "{\"id\": \"1363\", \"js_event_callbacks\": {}, \"js_property_callbacks\": {}, \"name\": null, \"power_limit_high\": 5, \"power_limit_low\": -3, \"precision\": \"auto\", \"subscribed_events\": [], \"tags\": [], \"use_scientific\": true}");
     }
 }
