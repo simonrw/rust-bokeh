6b10eeb58a18152baf5da71a38362c15ff1e64d2
Simon Walker <s.r.walker101@googlemail.com>
2018-11-20 22:12:05 +0000

    Swap internal error type for failure::Error;

--- bokeh-models/src/errors.rs
+++ bokeh-models/src/errors.rs
@@ -1,3 +1,3 @@
-use std::error::Error;
+use failure::Error;
 
-pub type Result<T> = std::result::Result<T, Box<dyn Error>>;
+pub type Result<T> = std::result::Result<T, Error>;
--- bokeh-models/src/lib.rs
+++ bokeh-models/src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate serde_json;
+extern crate failure;
 #[cfg(test)]
 extern crate rand;
 
